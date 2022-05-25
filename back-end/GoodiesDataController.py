76458ab7637e6b820ed84be692d31807e91b83dd
Charline bch <charline.bouche@epsi.fr>
2022-05-25 10:02:41 +0200

    GDS-002 : update

--- back-end/GoodiesDataController.py
+++ back-end/GoodiesDataController.py
@@ -8,20 +8,9 @@ class Product(BaseModel):
     type: str
     photo_URL: str
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": "1",
-                "name": "casquette toto",
-                "type": "vêtement",
-                "photo_URL": "front-end/images/casquette.jpeg",
-            }
-        }
-
-
 product_list = [Product(id=1, name='Casquette toto', type='vêtement', photo_URL="casquette.jpeg"),
                 Product(id=2, name='Casquette', type='vêtement', photo_URL="casquette.jpeg"),
-                Product(id=3, name='Gourde', type='bouteille', photo_URL="gourde.jpeg"),
+                Product(id=3, name='Gourde', type='objet', photo_URL="gourde.jpeg"),
                 Product(id=4, name='Sac à dos', type='vêtement', photo_URL="sac-a-dos.jpeg"),
                 Product(id=5, name='t-shirt 1', type='vêtement', photo_URL="t-shirt.png"),
                 Product(id=6, name='t-shirt 2', type='vêtement', photo_URL="t-shirt.png"),
@@ -29,7 +18,7 @@ product_list = [Product(id=1, name='Casquette toto', type='vêtement', photo_URL
                 Product(id=8, name='t-shirt 4', type='vêtement', photo_URL="t-shirt.png"),
                 Product(id=9, name='Sweatshirt', type='vêtement', photo_URL="sweatshirt.jpeg"),
                 Product(id=10, name='Sweatshirt', type='vêtement', photo_URL="sweatshirt.jpeg"),
-                Product(id=11, name='Mug', type='vêtement', photo_URL="mug.jpeg")]
+                Product(id=11, name='Mug', type='objet', photo_URL="mug.jpeg")]
 
 
 @app.get("/product", tags=["Produit"])
