aa1a00c7df450df973a8ebe40a46bd59d9b708a8
Charline bch <charline.bouche@epsi.fr>
2022-05-27 12:25:27 +0200

    GDS-004 : add form connexion

--- front-end/login.php
+++ front-end/login.php
@@ -1,1 +1,47 @@
 <?php
+$title = "Connexion";
+include_once "header.php";
+?>
+
+    <section id="homepage">
+
+        <div class="container">
+
+            <h1>SHOODIES</h1>
+
+            <form action="?" method="POST" enctype="multipart/form-data">
+
+                <div class="form-field">
+                    <label for="mail"></label>
+                    <input type="text"
+                           class="form-control"
+                           id="mail"
+                           name="mail"
+                           placeholder="jean@shodo-nantes.io"
+                           value=""
+                           maxlength="255"
+                           required></div>
+
+                <div class="form-field">
+                    <label for="password"></label>
+                    <input type="password"
+                           class="form-control"
+                           id="password"
+                           name="password"
+                           placeholder="password"
+                           value=""
+                           maxlength="20"
+                           required></div>
+
+                <div class="form-field">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+
+            </form>
+
+        </div>
+
+    </section>
+
+<?php
+include_once "footer.php";
