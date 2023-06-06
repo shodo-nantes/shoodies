1dcaeea9c9d0ceaafe34745a003670074b00b4b3
clamc068 <laetitia.caffarelli@gmail.com>
2023-06-06 11:03:10 +0200

    delete changes in home.tsx

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,9 +1,7 @@
-import Typography from '@mui/material/Typography';
-
 export default function Home() {
     return (
         <>
-            <Typography variant="h1">Shoodies</Typography>;
+            <h1>Home</h1>
         </>
     );
 }
