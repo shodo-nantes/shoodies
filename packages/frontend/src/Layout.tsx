74a91260b5d60df56e2f5a37f2221bffbc8ce04a
clamc068 <laetitia.caffarelli@gmail.com>
2023-06-06 11:54:14 +0200

    add appbar with link to Home

--- packages/frontend/src/Layout.tsx
+++ packages/frontend/src/Layout.tsx
@@ -1,10 +1,28 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
     return (
         <>
             <Container maxWidth="lg" component="main" role="main">
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography
+                                variant="h6"
+                                component="a"
+                                href="/Home"
+                                sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}
+                            >
+                                Shoodies
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
                 <Outlet />
             </Container>
         </>
