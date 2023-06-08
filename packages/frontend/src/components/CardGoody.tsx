85b7270dbfb3e7e56e9f24eaece43ad0d7ec2448
Laetitia Caffarelli <113118386+LCaffarelli@users.noreply.github.com>
2023-06-08 11:30:30 +0200

    add package, rename some and attempt multiply card (#14)
    * add a package for the interface Goody+ rename CardComponent into CardGoody+ add attempt to add multiple card in home
    
    * add the possibilty to create multiple card + needs to add grid to align it perfectly
    
    * add the possibilty to create multiple card + needs to add grid to align it perfectly

--- packages/frontend/src/components/CardGoody.tsx
+++ packages/frontend/src/components/CardGoody.tsx
@@ -3,28 +3,31 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-export default function CardComponent() {
+import { Goody } from 'types/goody';
+
+export default function CardGoody(props: Goody) {
+    const { image } = props;
+    const { name } = props;
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
+                id="image"
                 sx={{
                     maxWidth: 345,
                 }}
                 component={'img'}
-                image="https://images.unsplash.com/photo-1685900337313-64cd27f0238e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
-                title="a hand holding a pink poppy"
+                image={image}
             />
             <CardContent>
                 <Typography
                     gutterBottom
-                    variant="h5"
-                    component="div"
+                    component="h5"
+                    id="name"
                     sx={{
                         textAlign: 'center',
-                        fontSize: 25,
                     }}
                 >
-                    Nom du produit
+                    {name}
                 </Typography>
             </CardContent>
         </Card>
--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import { Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
-import CardComponent from 'components/Card';
+import CardGoody from 'components/CardGoody';
+import { Goody } from 'types/goody';
+
 export default function Home() {
+    const listGoodies: Goody[] = [
+        {
+            image: 'https://images.unsplash.com/photo-1685900337313-64cd27f0238e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80',
+            name: 'fleur',
+        },
+        {
+            image: 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/7/4/3/743f5af537_50017360_meduse-dags1974-flickr-03.jpg',
+            name: 'méduses',
+        },
+        {
+            image: 'http://angelloretta1974.a.n.pic.centerblog.net/o/9f7f0359.jpg',
+            name: 'papillon',
+        },
+    ];
+
     return (
         <>
             <Typography
@@ -12,7 +30,11 @@ export default function Home() {
             >
                 Shoodies
             </Typography>
-            <CardComponent />
+            <Container>
+                {listGoodies.map((goodies) => (
+                    <CardGoody key={(goodies.image, goodies.name)} image={goodies.image} name={goodies.name} />
+                ))}
+            </Container>
         </>
     );
 }
