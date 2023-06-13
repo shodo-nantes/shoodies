c784a1ce6ac1d4a9fe64b5a9d4f5351767e4944f
clamc068 <laetitia.caffarelli@gmail.com>
2023-06-13 09:42:32 +0200

    add SuperTest and Jest + add config for Jest + remove test in app.ts + add package and file for tests + add script to run test

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,8 @@
 import express, { Express, Request, Response } from 'express';
-import supertest from 'supertest';
 
 import { getGoodies } from 'repositories/GoodiesRepository';
 const app: Express = express();
-const requestSuperTest = supertest;
+
 app.use(express.json());
 
 app.get('/api/goodies', async (request: Request, response: Response) => {
@@ -11,10 +10,4 @@ app.get('/api/goodies', async (request: Request, response: Response) => {
     response.send(goodies);
 });
 
-requestSuperTest('http://localHost:3000')
-    .get('/api/goodies')
-    .expect(200)
-    .end(function (error, response) {
-        if (error) throw new error();
-    });
 export default app;
---
+++ packages/backend/tests/api/api.test.ts
@@ -0,0 +1,38 @@
+import app from 'app';
+import supertest from 'supertest';
+
+const request = supertest;
+
+describe('Api', () => {
+    it('should return a mug in a list of goodies', async () => {
+        const result = await request(app).get('/api/goodies').expect(200);
+        expect(result.body).toContainEqual(expect.objectContaining({ name: 'Mug' }));
+    });
+
+    it('should return a list of goodies', async () => {
+        const result = await request(app).get('/api/goodies').expect(200);
+        const expected = [
+            {
+                name: 'Mug',
+                image: 'https://placehold.co/600x400',
+            },
+            {
+                name: 'T-Shirt',
+                image: 'https://placehold.co/500x400',
+            },
+            {
+                name: 'Sticker',
+                image: 'https://placehold.co/600x300',
+            },
+            {
+                name: 'Hoodie',
+                image: 'https://placehold.co/450x200',
+            },
+            {
+                name: 'Casquette',
+                image: 'https://placehold.co/550x350',
+            },
+        ];
+        expect(result.body).toEqual(expected);
+    });
+});
