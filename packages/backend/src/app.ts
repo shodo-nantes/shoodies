d0ba4b744b0bbc916466ddb74bca8d99fe4337fd
Joël Piazzalunga-Lerat <joel.piazzalungalerat@gmail.com>
2023-07-06 12:25:53 +0200

    Feature/gds 999 (#19)
    * [GDS-999] Add CORS and a call to the backend for deployment testing
    
    * [GDS-999] Use backend url from .env
    
    * feat(backend): add docker:build and docker:run scripts to package.json to build and run the app in a container
    
    * Add github action for CD
    
    * Update CD
    
    * cd: Update cd with env variable for gcp connection
    
    * feat: automatic deployment
    
    ---------
    
    Co-authored-by: Thomas Carpaye <thomas.carpaye@gmail.com>
    Co-authored-by: mbonnot <bonnot.matthieu@gmail.com>

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
 
 import { getGoodies } from 'repositories/GoodiesRepository';
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/goodies', async (request: Request, response: Response) => {
     const goodies = await getGoodies();
--- packages/frontend/src/Layout.tsx
+++ packages/frontend/src/Layout.tsx
@@ -1,11 +1,23 @@
 import Container from '@mui/material/Container';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import AppBarComponent from 'components/AppBar';
+
 export default function Layout() {
+    const [hello, setHello] = useState('toto');
+    useEffect(() => {
+        const message = async () => {
+            const rawResponse = await fetch(import.meta.env.VITE_API_URL);
+            const jsonResponse = await rawResponse.json();
+            setHello(jsonResponse.message);
+        };
+        message();
+    });
     return (
         <>
             <AppBarComponent />
+            <h1>{hello}</h1>
             <Container maxWidth="lg" component="main" role="main">
                 <Outlet />
             </Container>
