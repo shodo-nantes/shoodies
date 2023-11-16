720a0bd18a8b8f8f8a4f6d65a2a78acf530e3732
Mara Martini <mma_@gmail.com>
2023-11-16 15:59:51 +0100

    JS modified - effect working only on home page

--- packages/frontend/script.js
+++ packages/frontend/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Rimuovi l'overlay dopo un secondo e mezzo
-  setTimeout(function () {
-    var overlay = document.getElementById("overlay");
-    overlay.parentNode.removeChild(overlay);
+  if (window.location.pathname.includes("index.html")) {
+    setTimeout(function () {
+      var overlay = document.getElementById("overlay");
+      overlay.parentNode.removeChild(overlay);
 
-    // Rendi visibile il resto del contenuto
-    document.querySelector("main").style.opacity = 1;
-  }, 2000);
+      document.querySelector("main").style.opacity = 1;
+    }, 2000);
+  }
 });
