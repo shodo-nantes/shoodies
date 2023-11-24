16775c05b9db6506f067ea62a59b7497fe6bb339
Mara Martini <mma_@gmail.com>
2023-11-24 16:32:18 +0100

    React reparation

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Header from "./components/Header";
-import CardList from "./components/CardList";
-import Footer from "./components/Footer";
-import Article from "./components/Article";
+import Article from './components/Article';
+import CardList from './components/CardList';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <Router>
-      <Header />
+    return (
+        <Router>
+            <Header />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<CardList />} />
-          <Route path="/article/:id" element={<Article />} />
-        </Routes>
-      </main>
+            <main>
+                <Routes>
+                    <Route path="/" element={<CardList />} />
+                    <Route path="/article/:id" element={<Article />} />
+                </Routes>
+            </main>
 
-      <Footer />
-    </Router>
-  );
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
