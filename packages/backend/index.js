cc0b0d55ef30bf6c48079d08152bc84053092549
Mara Martini <mma_@gmail.com>
2023-11-27 13:44:20 +0100

    Import instead of const express

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Ciao');
+app.get('/', (request, response) => {
+    response.send('Ciao');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    // console.log(`Example app listening on port ${port}`);
 });
 
-module.exports = app;
+export default app;
